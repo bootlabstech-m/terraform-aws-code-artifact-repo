46ad989423e35299ada30e897a1c21c37e5fb97a
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-03 15:25:35 +0530

    added lifecycle block

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 resource "aws_kms_key" "encryption_key" {
   description = var.key_name
+    lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "aws_codeartifact_domain" "domain" {
   domain         = var.domain_name
   encryption_key = aws_kms_key.encryption_key.arn
+    lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "aws_codeartifact_repository" "codeartifact_repo" {
   repository = var.artifacts_repository_name
   domain     = aws_codeartifact_domain.domain.domain
+    lifecycle {
+    ignore_changes = [tags]
+  }
 }
